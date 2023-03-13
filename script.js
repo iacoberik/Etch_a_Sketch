ba10797ea245e69ab4c93da78c97572318b39a92
iacoberik <iacob.erik320@gmail.com>
2023-03-13 14:38:56 +0200

    grid selection added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container_table');
+const gridSelector = document.querySelector("input[name='grid']");
+let gridSelectorNumber = gridSelector.valueAsNumber;
 
-const createGrid = (grid) =>{
+const createGrid = (grid) => {
     squareGrid = grid * grid;
+    container.style.gridTemplateColumns = `repeat(${grid} ,1fr)`;
     let i = 0;
     while (i < squareGrid){
         let div = document.createElement('div');
@@ -11,12 +14,28 @@ const createGrid = (grid) =>{
         i++;
     }
 }
-createGrid(16);
 
-const gridElements = document.querySelectorAll('div.grid-element');
 function changeBackgroud(){
     this.classList.add('hoverd');
     this.removeEventListener('mouseenter', changeBackgroud);
 }
 
-gridElements.forEach( element => element.addEventListener('mouseenter', changeBackgroud))
+function cb(){
+    gridSelectorNumber = this.valueAsNumber;
+    container.innerHTML = ''; 
+    createGrid(gridSelectorNumber); 
+    let gridElements = document.querySelectorAll('.grid-element');
+    gridElements.forEach( element => element.addEventListener('mouseenter', changeBackgroud));
+}
+
+gridSelector.addEventListener('change', cb);
+
+
+
+
+
+
+
+
+
+
